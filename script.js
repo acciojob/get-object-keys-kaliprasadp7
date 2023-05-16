bcb883e47f3346c983c7d0ba03c262e690548e4e
Kali Prasad Panda <kaliprasadp7@gmail.com>
2023-05-16 20:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let obj = {
-	name : 'Sikun',
-	class : 12
-}
+// Create the student object
+const student = {
+  name: 'John Doe'
+};
 
-function getKeys(obj) {
-	return Object.keys(obj);
-}
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
 
-console.log(getKeys());
+// Access the keys of the student object
+const keys = student.getKeys();
+
+// Output the keys
+console.log(keys);
